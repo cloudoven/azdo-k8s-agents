464b9675abeea22543c5a72ce9649365be3698b8
Moim HA <moimhossain@gmail.com>
2022-05-19 12:38:15 +0200

    Clean ups

--- src/kubernetes/AgentController/Kubes/CrdConstants.cs
+++ src/kubernetes/AgentController/Kubes/CrdConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AgentController.Kubes
+﻿namespace AgentController.Kubes
 {
     public class CrdConstants
     {
